fb2ebf3aec49d447b0b8b9955ad1373734c6032f
Georgi Georgiev <gogeorgievgo@gmail.com>
2025-03-12 13:33:14 +0200

    Modified the validations in ProductRequest and extended DtoMapper with a method fromProduct which is used for the updateProduct functionality in the app

--- src/main/java/com/footballstore/web/dto/ProductRequest.java
+++ src/main/java/com/footballstore/web/dto/ProductRequest.java
@@ -3,7 +3,9 @@ package com.footballstore.web.dto;
 import com.footballstore.product.model.Brand;
 import com.footballstore.product.model.Category;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,10 +18,10 @@ import java.math.BigDecimal;
 
 @AllArgsConstructor
 public class ProductRequest {
-    @NotNull(message = "Name is required.")
+    @NotBlank(message = "Name is required.")
     private String name;
 
-    @NotNull(message = "Description is required.")
+    @Size(min = 10, max = 1000, message = "Description length must be between 10 and 1000 characters!")
     private String description;
 
     @NotNull(message = "Price is required")
--- src/main/java/com/footballstore/web/mapper/DtoMapper.java
+++ src/main/java/com/footballstore/web/mapper/DtoMapper.java
@@ -1,6 +1,8 @@
 package com.footballstore.web.mapper;
 
+import com.footballstore.product.model.Product;
 import com.footballstore.user.model.User;
+import com.footballstore.web.dto.ProductRequest;
 import com.footballstore.web.dto.UserEditRequest;
 import lombok.experimental.UtilityClass;
 
@@ -13,6 +15,18 @@ public class DtoMapper {
                 .firstName(user.getFirstName())
                 .lastName(user.getLastName())
                 .phoneNumber(user.getPhoneNumber())
+                .build();
+    }
+
+    public static ProductRequest fromProduct(Product product) {
+        return ProductRequest.builder()
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .category(product.getCategory())
+                .imageUrl(product.getImageUrl())
+                .brand(product.getBrand())
+                .isInStock(product.isInStock())
                 .build();
     }
 }
