251eff8fc3e0b5e329ef675dc921462b929f86eb
Georgi Georgiev <gogeorgievgo@gmail.com>
2025-03-22 01:51:30 +0200

    Following naming convention, renamed application class from OnlineFootballApplication to Application

--- src/main/java/com/footballstore/Application.java
+++ src/main/java/com/footballstore/Application.java
@@ -8,10 +8,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFeignClients
 @EnableScheduling
 @SpringBootApplication
-public class OnlineFootballStoreApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineFootballStoreApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
